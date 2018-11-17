494142f0bdd7ff254f9dab9e4489ed0222116fb4
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2018-11-17 17:08:12 -0500

    Add Toggle Switch

---
+++ platforms/browser/www/js/validation.js
@@ -0,0 +1,8 @@
+function validateEmail(email) {
+	var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return regex.test(email);
+}
+function validatePassword(pass) {
+	var regex = RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+	return regex.test(pass);
+}
