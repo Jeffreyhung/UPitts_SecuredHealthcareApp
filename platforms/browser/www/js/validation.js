67eab86a296a8577c021d98db96eb0c6a40c7a5f
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2018-11-18 17:48:49 -0500

    Add input validation

--- platforms/browser/www/js/validation.js
+++ platforms/browser/www/js/validation.js
@@ -6,7 +6,23 @@ function validatePassword(pass) {
 	var regex = RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
 	return regex.test(pass);
 }
-function regularInput(words) {
-	var regex = /[a-zA-Z0-9.,?]*/;
+function validateStictInput(words) {
+	var regex = /^[_A-z0-9]+((-|\s)*[._A-z0-9])*$/;
 	return regex.test(words)
+}
+function validateInput(words) {
+	var regex = /^[_A-z0-9]+((-|\s)*[_A-z0-9])*$/;
+	return regex.test(words)
+}
+function validateNumbers(numbers) {
+	var regex = /^[0-9]+$/;
+	return regex.test(numbers)
+}
+function validateDate(date) {
+	var regex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+	return regex.test(date)
+}
+function validatePhone(phone) {
+	var regex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-]{0,1}[0-9]{3}[-]{0,1}[0-9]{4}$/;
+	return regex.test(phone);
 }
