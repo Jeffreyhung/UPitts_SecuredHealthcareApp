50935bb9371a6e40e2633c0a1c156dd811ec589e
Jeffreyhung <jeffreyhung@hotmail.com.tw>
2018-11-30 21:00:39 -0500

    complete show data

--- platforms/browser/www/js/health.js
+++ platforms/browser/www/js/health.js
@@ -9,7 +9,7 @@ function afterRTFS() {
 
 function fileExists(fileEntry) {
     loadFile("medicalInfo", PFS);
-    document.getElementById("defaultMessage").style.display = 'block';
+    document.getElementById("defaultMessage").style.display = 'none';
 }
 
 function fileDoesNotExist() {
@@ -43,11 +43,32 @@ function loadSessionSuccess(content) {
 
 function showData(info) {
     var ul = document.getElementById("dataTable");
+    var temp="";
     for (i in info.data) {
         console.log(info.data[i]);
         var li = document.createElement("li");
+        var p = document.createElement("p");
         li.className  = "table-view-cell";
-        li.appendChild(document.createTextNode(info.data[i]));
+        li.appendChild(document.createTextNode(info.data[i].date));
+
+        temp = "Hospital : " + info.data[i].hospital;
+        p.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
+        p.appendChild(document.createTextNode(temp));
+        p.appendChild(document.createElement("br"));
+        temp = "Doctor : " + info.data[i].doctor;
+        p.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
+        p.appendChild(document.createTextNode(temp));
+        p.appendChild(document.createElement("br"));
+        temp = "Problem : " + info.data[i].problem;
+        p.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
+        p.appendChild(document.createTextNode(temp));
+        p.appendChild(document.createElement("br"));
+        temp = "Threatmeat : " + info.data[i].threatment;
+        p.appendChild(document.createTextNode('\u00A0\u00A0\u00A0'));
+        p.appendChild(document.createTextNode(temp));
+        p.appendChild(document.createElement("br"));
+        var temp =null;
+        li.appendChild(p);
         ul.appendChild(li);
     }
 }
